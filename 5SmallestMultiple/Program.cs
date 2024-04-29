b5d23c4da15852422fddc4a24b368aa56ffc4f48
mikecabana <mikeyc343@gmail.com>
2024-04-29 15:46:29 -0400

    5 done

---
+++ 5SmallestMultiple/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var result = SmallestMultiple(20);
+        Console.WriteLine($"Smallest Multiple: {result}");
+    }
+
+    public static long SmallestMultiple(int max)
+    {
+        long num = 1;
+        while(true)
+        {
+            for (var i = 1; i <= max; i++)
+            {
+                if (num % i != 0)
+                {
+                    num += 1;
+                    i = 1;
+                }
+
+            }
+            break;
+
+        }
+        return num;
+    }
+}
